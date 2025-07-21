41e3a93b042d446bad463f4204bf5413638316f3
Bohdan <burchakk.bohdan@gmail.com>
2025-07-21 15:42:21 +0300

    remove nanoid library. Add create random lobby ID

--- server/utils/generateLobbyID.js
+++ server/utils/generateLobbyID.js
@@ -1,7 +1,19 @@
-const { customAlphabet } = require("nanoid");
-const alphabet =
-  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+// const { customAlphabet } = require("nanoid");
+// const alphabet =
+//   "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-const generateLobbyID = customAlphabet(alphabet, 5);
+// const generateLobbyID = customAlphabet(alphabet, 5);
+
+// module.exports.generateLobbyID = generateLobbyID;
+function generateLobbyID() {
+  const alphabet =
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let id = "";
+  for (let i = 0; i < 5; i++) {
+    const randomIndex = Math.floor(Math.random() * alphabet.length);
+    id += alphabet[randomIndex];
+  }
+  return id;
+}
 
 module.exports.generateLobbyID = generateLobbyID;
